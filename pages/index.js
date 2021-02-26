e91c2974a220819ff2fadbd48972f3e78efe6f22
ylu <ylu@ert.com>
2021-02-25 22:18:00 -0500

    init

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+const Index = () => {
+  return (
+    <div>
+      <p>Sample app using react and next.js</p>
+    </div>
+  )
+}
+
+export default Index
+
+// https://shopify.dev/tutorials/build-a-shopify-app-with-node-and-react/embed-your-app-in-shopify
+
+// ngrok http 3000
+// You can create three different types of apps for Shopify stores: public, custom and private. 
+// here: public
